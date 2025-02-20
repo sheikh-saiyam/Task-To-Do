a17917580d818899391c55f2afdb6fa843fa38e4
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-02-20 23:47:46 +0600

    delete task done

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ async function run() {
     });
 
     // Delete a task --->
-    app.delete("/delete-tasks/:id", async (req, res) => {
+    app.delete("/delete-task/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await tasksCollection.deleteOne(query);
